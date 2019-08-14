28e07b7091fbefa18662bd96226920179f32d16e
Michal Vala <sparkoo@users.noreply.github.com>
2019-08-14 06:33:29 +0200

    script to update docs schema (#49)
    Signed-off-by: Michal Vala <mvala@redhat.com>

---
+++ docs/updateSchema.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#set -x
+
+if [ -z "${1}" ]; then
+  echo "use script like this: 'sh updateSchema.sh [path_to_devfile.json]'"
+  exit 1
+fi
+
+if [ ! -f "${1}" ]; then
+    echo "File '${1}' does not exist."
+    exit 1
+fi
+
+if [ ! -x "$( command -v docker )" ]; then
+  echo 'Docker not found. Install to run this script.'
+  exit 1
+fi
+
+DEVFILE_JSON=$1
+REPO_DIR=$( dirname "${0}" )
+REPO_DIR=$( realpath "${REPO_DIR}")
+
+docker run -it --rm \
+-v "${DEVFILE_JSON}":/home/node/devfile.json:Z \
+-v "${REPO_DIR}":/home/node/devfile:Z \
+node /bin/bash -c \
+"npm install -g @adobe/jsonschema2md; jsonschema2md -d /home/node/devfile.json -o /home/node/devfile;"
